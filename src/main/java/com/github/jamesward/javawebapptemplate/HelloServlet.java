b0377422b622d23e8c50b72abe6465b67a1086ad
James Ward <james@jamesward.org>
2012-01-23 14:46:22 -0700

    init

---
+++ src/main/java/com/github/jamesward/javawebapptemplate/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.github.jamesward.javawebapptemplate;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet
+{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        PrintWriter out = resp.getWriter();
+        out.println("hello, world");
+        out.close();        
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h1>Hello, <%="JSP"%></h1>
+<a href="/hello">Hello, Servlet</a>
+</body>
+</html>
